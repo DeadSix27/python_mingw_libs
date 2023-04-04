1013786aa8e1506ed1aa36e5ba5f2f54f6b20ed3
DeadSix27 <DeadSix27@users.noreply.github.com>
2023-04-05 01:58:34 +0200

    Update install_python_libs.py

--- install_python_libs.py
+++ install_python_libs.py
@@ -116,7 +116,6 @@ else:
 		run_cmd('unzip -po {0} {1}.dll >{1}.dll'.format(filename,dllname))
 		run_cmd('unzip -po {0} libffi-8.dll >libffi-8.dll'.format(filename))
 		run_cmd('unzip -po {0} _ctypes.pyd >_ctypes.pyd'.format(filename))
-		run_cmd('unzip -po {0} libffi-7.dll >libffi-7.dll'.format(filename))
 		run_cmd('unzip -po {0} {1}.zip >{1}.zip'.format(filename,dllname))
 		
 		print("Local installing dll")
